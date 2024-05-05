c746fadacf4ebd97439995bee2ed1abe0a9a6e4b
Mathias Törnblom <mathias@tornbloms.net>
2024-05-05 13:26:09 +0200

    Initial Commit

---
+++ Namnsdagar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Namnsdagar
+//
+//  Created by Mathias Törnblom on 2024-05-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Namnsdagar/NamnsdagarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamnsdagarApp.swift
+//  Namnsdagar
+//
+//  Created by Mathias Törnblom on 2024-05-05.
+//
+
+import SwiftUI
+
+@main
+struct NamnsdagarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
