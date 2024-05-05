ac1217a4404d3e273c6dc7c233ee1c0ce3cd423f
Mathias Törnblom <mathias@tornbloms.net>
2024-05-05 21:05:24 +0200

    Datepicker working and swiping

---
+++ Namnsdagar/View/CalendarPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarPickerView.swift
+//  Namnsdagar
+//
+//  Created by Mathias Törnblom on 2024-05-05.
+//
+
+import SwiftUI
+
+struct CalendarPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarPickerView()
+}
