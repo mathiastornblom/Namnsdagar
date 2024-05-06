975f8443ba03993a2e1e47a43aa2f0e5f7de8edc
Mathias Törnblom <mathias@tornbloms.net>
2024-05-07 00:03:22 +0200

    Respons if seach could not be found

--- Namnsdagar/View/SearchView.swift
+++ Namnsdagar/View/SearchView.swift
@@ -12,34 +12,46 @@ struct SearchView: View {
   @Binding var currentDate: Date
   @Binding var isPresented: Bool // Add this to control the view presentation
 
-  @FocusState var focused: Bool?
+  @FocusState private var isSearchFocused: Bool
   @State private var searchQuery: String = ""
   @State private var searchResults: [(name: String, date: Date)] = []
+    @State private var hasSearched: Bool = false  // Track if a search has been performed
     
   var body: some View {
     NavigationView {
       List {
           TextField("Enter a name", text: $searchQuery)
             .textFieldStyle(RoundedBorderTextFieldStyle())
-            .focused($focused, equals: true)
+            .focused($isSearchFocused)
             .onChange(of: searchQuery) { oldValue, newValue in
-              // Check if the search query has actually changed
-              if oldValue != newValue {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { // Delay for 0.1 seconds
-                  searchResults = viewModel.findAllOccurrences(of: newValue)
+                if newValue.isEmpty {
+                    searchResults = []
+                    hasSearched = false  // Reset the search status
+                } else {
+                    searchResults = viewModel.findAllOccurrences(of: newValue)
+                    hasSearched = true  // Update search status to true
                 }
-              }
-              self.focused = true // Ensure focus remains on TextField
             }
-
-        ForEach(searchResults, id: \.name) { result in
-          Button(action: {
-            currentDate = result.date
-            isPresented = false // Dismiss the view
-          }) {
-            Text("\(result.name) - \(result.date, formatter: dateFormatter)")
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { // Delay for 0.1 seconds
+                    self.isSearchFocused = true // Ensure focus remains on TextField
+              }
+            
+            }
+          // Check if results are empty and a search has been performed
+          if searchResults.isEmpty && hasSearched {
+              Text("No results found for '\(searchQuery)'. Please try another name.")
+                  .foregroundColor(.red)
+          } else {
+              ForEach(searchResults, id: \.name) { result in
+                  Button(action: {
+                      currentDate = result.date
+                      isPresented = false // Dismiss the view
+                  }) {
+                      Text("\(result.name) - \(result.date, formatter: dateFormatter)")
+                  }
+              }
           }
-        }
       }
       .navigationBarTitle("Search", displayMode: .inline)
     }
